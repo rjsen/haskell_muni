c011aa8157e6a1dae7bb4a5240019dd94cdd77cd
Ramesh Sen <rjsen@me.com>
2011-08-19 18:10:32 -0700

    more work

--- muni.hs
+++ muni.hs
@@ -4,11 +4,18 @@ import Network.Mail.Mime
 
 recip = "4082300301@txt.att.net"
 needed = 5
+sid = "16619"
+rt = "L"
 
-res <- simpleHTTP getRequest("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId=16619&routeTag=L")
+msg = "To: " ++ recip ++ "\nThe next " ++ rt ++ " will be at stop " ++ sid ++ " in "
+
+res <- simpleHTTP getRequest("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId=" ++ sid ++ "&routeTag=" ++ rt)
     
 xml <- getResponseBody res
 
 Just doc = parseXMLDoc xml
 Just min = findAttr (QName "minutes" Nothing Nothing ) (head $ findElements (QName "prediction" Nothing Nothing ) doc)
 
+checkTime :: (Int a) => Int -> Bool
+checkTime needed = 
+checkTime x = False
